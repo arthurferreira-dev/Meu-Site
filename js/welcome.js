33d9d2419ff8a0ff1095cce5a5dde32b3827a750
Arthur Ferreira <tutudacosta996@gmail.com>
2025-05-10 21:07:49 -0300

    version 0.1.9 (btn-correct)

--- js/welcome.js
+++ js/welcome.js
@@ -1,16 +1,21 @@
 function Name() {
     let txt = document.getElementById('title')
+    let btn = document.getElementById('name-btn')
     let name = prompt('Olá, qual é seu nome?')
 
+    btn.style.display = 'block'
     txt.style.textAlign = 'center'
     txt.style.marginTop = '6px'
     txt.style.marginBottom = '6px'
 
     if (name == '') { //Sem Nome
         txt.innerHTML = `Olá convidado, seja bem-vindo.`
+        btn.style.display = 'none'
     } else if (name == 'Multgames' || name == 'Multhingss') { //Nome Multhingss
         txt.innerHTML = `Olá Multhingss, tudo certo?`
+        btn.style.display = 'none'
     } else { //Nome Padrão
         txt.innerHTML = `Olá ${name}, seja bem-vindo(a).`
+        btn.style.display = 'none'
     }
 }
